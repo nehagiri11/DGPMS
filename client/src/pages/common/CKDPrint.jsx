3e37fb8b7dd24a867e08c43c5d8d80a25d21cbfd
nehagiri11 <inehagiri11@gmail.com>
2026-06-24 09:32:32 +0545

    Improve print layout spacing and section alignment

--- client/src/pages/common/CKDPrint.jsx
+++ client/src/pages/common/CKDPrint.jsx
@@ -198,7 +198,7 @@ function CKDPrint({ request }) {
 
       {renderCopy("SECURITY COPY - 1", true)}
 
-      <div className="cut-line border-t-2 border-dotted border-black my-6 relative">
+      <div className="cut-line border-t-2 border-dotted border-black my-10 relative">
         <span className="absolute left-1/2 -translate-x-1/2 -top-3 bg-white px-3 text-xs font-semibold">
           Cut here
         </span>
